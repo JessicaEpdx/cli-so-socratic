e535f75ec0d90ef09546e10e12b835dab96ad7e8
Guest User <student@epicodus.com>
2015-06-19 09:45:43 -0700

    allow user to reply with answer, shows answers on question page and links to individual answer page

---
+++ app/controllers/question.js
@@ -0,0 +1,35 @@
+import Ember from 'ember';
+
+var QuestionController = {
+  isAnswering: false,
+  needs: ['question'],
+  actions: {
+    addAnswer: function(){
+      this.set('isAnswering', true);
+    },
+    saveAnswer: function(){
+      var newAnswer = this.store.createRecord('answer', {
+        answerName: this.get('answerName'),
+        answerTitle: this.get('answerTitle'),
+        answerDescription: this.get('answerDescription'),
+        answerImage: this.get('answerImage')
+
+      });
+      newAnswer.save();
+
+      var question = this.get('controllers.question.model');
+      question.get('answers').pushObject(newAnswer);
+      question.save();
+
+      this.set('answerName', "");
+      this.set('answerDescription', "");
+      this.set('answerImage', "");
+      this.set('answerTitle', "");
+
+      this.set('isAnswering', false);
+
+    }
+  }
+};
+
+export default Ember.Controller.extend(QuestionController);
--- app/router.js
+++ app/router.js
@@ -8,7 +8,10 @@ var Router = Ember.Router.extend({
 Router.map(function() {
   this.resource('questions', {path: '/'});
   this.resource('question', {path: 'questions/:question_id'});
-
+  this.resource('edit-questions');
+  this.resource('edit-question', {path: 'edit-questions/:question_id'});
+  this.resource('answer', {path: 'answers/:answer_id'});;
+  this.resource('answers');
 });
 
 export default Router;
